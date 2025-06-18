3b3f1a65f4392b3847c1d7b8cd6a15bdf7ad8344
Mitch Allen <github@mitchallen.com>
2025-06-18 17:22:40 -0400

    feat: updated babel and grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function (grunt) {
                     browserifyOptions: {
                         standalone: 'MitchAllen.GridCore'
                     },
-                    transform: [['babelify', {presets: ['es2015']}]],
+                    transform: [['babelify', {presets: ['@babel/preset-env']}]],
                     plugin: [[ "browserify-derequire" ]]
                 },
                 files: {
--- dist/grid-core.js
+++ dist/grid-core.js
@@ -10,73 +10,67 @@
 "use strict";
 
 module.exports.create = function () {
-    var spec = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-    var _spec$rows = spec.rows,
-        _rows = _spec$rows === undefined ? 0 : _spec$rows;
-
-    _rows = Math.max(_rows, 0);
-
-    var _array = [];
-    while (_array.push([]) < _rows) {}
-
-    var obj = Object.create({}, {
-        "rows": {
-            writeable: false,
-            value: _rows,
-            enumerable: true
+  var spec = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var _spec$rows = spec.rows,
+    _rows = _spec$rows === void 0 ? 0 : _spec$rows;
+  _rows = Math.max(_rows, 0);
+  var _array = [];
+  while (_array.push([]) < _rows);
+  var obj = Object.create({}, {
+    "rows": {
+      writeable: false,
+      value: _rows,
+      enumerable: true
+    }
+  });
+  return Object.assign(obj, {
+    log: function log() {
+      console.log("size: %d: ", _rows);
+      console.log(_array);
+    },
+    rowSize: function rowSize(row) {
+      if (row < 0 || row >= _rows) {
+        return 0;
+      }
+      return _array[row].length;
+    },
+    isCell: function isCell(a, b) {
+      var rs = this.rowSize(a);
+      return a >= 0 && a < _rows && b >= 0 && b < rs;
+    },
+    set: function set(a, b, value) {
+      // problem for sparse arrays
+      // if(!this.isCell(a,b)) { return false; }
+      if (a < 0 || b < 0) return false;
+      _array[a][b] = value;
+      return true;
+    },
+    get: function get(a, b) {
+      if (!this.isCell(a, b)) {
+        return null;
+      }
+      return _array[a][b];
+    },
+    fill: function fill(value) {
+      for (var row = 0; row < _rows; row++) {
+        var rs = this.rowSize(row);
+        for (var pos = 0; pos < rs; pos++) {
+          _array[row][pos] = value;
         }
-    });
-
-    return Object.assign(obj, {
-
-        log: function log() {
-            console.log("size: %d: ", _rows);
-            console.log(_array);
-        },
-        rowSize: function rowSize(row) {
-            if (row < 0 || row >= _rows) {
-                return 0;
-            }
-            return _array[row].length;
-        },
-        isCell: function isCell(a, b) {
-            var rs = this.rowSize(a);
-            return a >= 0 && a < _rows && b >= 0 && b < rs;
-        },
-        set: function set(a, b, value) {
-            // problem for sparse arrays
-            // if(!this.isCell(a,b)) { return false; }
-            if (a < 0 || b < 0) return false;
-            _array[a][b] = value;
-            return true;
-        },
-        get: function get(a, b) {
-            if (!this.isCell(a, b)) {
-                return null;
-            }
-            return _array[a][b];
-        },
-        fill: function fill(value) {
-            for (var row = 0; row < _rows; row++) {
-                var rs = this.rowSize(row);
-                for (var pos = 0; pos < rs; pos++) {
-                    _array[row][pos] = value;
-                }
-            }
-        },
-        cloneArray: function cloneArray() {
-            var _clone = [];
-            while (_clone.push([]) < _rows) {}
-            for (var row = 0; row < _rows; row++) {
-                var rs = this.rowSize(row);
-                for (var pos = 0; pos < rs; pos++) {
-                    _clone[row][pos] = _array[row][pos];
-                }
-            }
-            return _clone;
+      }
+    },
+    cloneArray: function cloneArray() {
+      var _clone = [];
+      while (_clone.push([]) < _rows);
+      for (var row = 0; row < _rows; row++) {
+        var rs = this.rowSize(row);
+        for (var pos = 0; pos < rs; pos++) {
+          _clone[row][pos] = _array[row][pos];
         }
-    });
+      }
+      return _clone;
+    }
+  });
 };
 
 },{}]},{},[1])(1)
