93b26a0be7518f3b5164a454742719a8ab1ffd09
Mitch Allen <github@mitchallen.com>
2025-06-18 17:33:30 -0400

    feat: resolved build issues

--- build.js
+++ build.js
@@ -4,9 +4,12 @@ esbuild.build({
   entryPoints: ['./src/index.js'],
   bundle: true,
   outfile: './dist/grid-core.js',
-  format: 'umd',
+  format: 'iife', // changed from 'umd' to 'iife'
   globalName: 'MitchAllen_GridCore',
   minify: true,
   sourcemap: true,
   target: ['es2015'],
-}).catch(() => process.exit(1));
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
